1be1279dec2a417dc4b786fee2f28a3fd456b624
Rahul Sur <rahul.sur@innRoad.com>
2019-05-08 19:38:28 +0530

    preparing for deploying in heroku

---
+++ src/environments/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAV-TO77WLmtvEfjDZUpzAuj163TbMPbb8",
+  authDomain: "my-todo-app-9feff.firebaseapp.com",
+  databaseURL: "https://my-todo-app-9feff.firebaseio.com",
+  projectId: "my-todo-app-9feff",
+  storageBucket: "my-todo-app-9feff.appspot.com",
+  messagingSenderId: "682633533254",
+  appId: "1:682633533254:web:68b31b350e2f5154"
+};
